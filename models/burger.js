b5bd105735e4434ea46a631e7f14b9073d9d97f0
Parashara <parashara@gmail.com>
2019-01-03 14:09:17 -0800

    got sequelize to work properly

--- models/burger.js
+++ models/burger.js
@@ -1,24 +1,41 @@
 // *** Include Modules: orm.js
-var orm = require("../config/orm.js");
+// var orm = require("../config/orm.js");
 
-var burger = {
-  selectAll: function(cb) {
-    orm.selectAll("burgers", function(res) {
-      cb(res);
-    });
-  },
-  // The variables cols and vals are arrays.
-  insertOne: function(cols, vals, cb) {
-    orm.insertOne("burgers", cols, vals, function(res) {
-      cb(res);
-    });
-  },
-  updateOne: function(objColVals, condition, cb) {
-    orm.updateOne("burgers", objColVals, condition, function(res) {
-      cb(res);
-    });
-  }
-};
+// var burger = {
+//   selectAll: function(cb) {
+//     orm.selectAll("burgers", function(res) {
+//       cb(res);
+//     });
+//   },
+//   // The variables cols and vals are arrays.
+//   insertOne: function(cols, vals, cb) {
+//     orm.insertOne("burgers", cols, vals, function(res) {
+//       cb(res);
+//     });
+//   },
+//   updateOne: function(objColVals, condition, cb) {
+//     orm.updateOne("burgers", objColVals, condition, function(res) {
+//       cb(res);
+//     });
+//   }
+// };
 
 // *** Export burger object for controller
-module.exports = burger;
+// module.exports = burger;
+
+module.exports = function(sequelize, DataTypes) {
+  var Burger = sequelize.define("Burger", {
+    name : {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    devoured : {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    }
+  });
+  return Burger;
+};
--- server.js
+++ server.js
@@ -23,7 +23,13 @@ app.use(routes);
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
-// *** Listener
-app.listen(PORT, function() {
-  console.log('App listening on PORT: ' + PORT);
+
+var db = require("./models");
+
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 });
