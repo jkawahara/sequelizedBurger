7902b62a9e672e5fa4873ddb1240da2fc0f633aa
Parashara <parashara@gmail.com>
2019-01-03 13:35:00 -0800

    changed db name in connection

--- config/connection.js
+++ config/connection.js
@@ -11,7 +11,7 @@ if (process.env.JAWSDB_URL) {
     port: 3306,
     user: "root",
     password: "",
-    database: "burgers_db"
+    database: "our_sequelized_burger_db"
   });
 }
 
--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,4 @@
 -- Drop, create & use database burgers_db
-DROP DATABASE IF EXISTS burgers_db;
-CREATE DATABASE burgers_db;
-USE burgers_db;
-
--- Create table burgers: burger_name, devoured, date 
-CREATE TABLE burgers (
-  id INT(4) AUTO_INCREMENT NOT NULL,
-  burger_name VARCHAR(100) NOT NULL,
-  devoured BOOLEAN DEFAULT false,
-  date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (id)
-);
+DROP DATABASE IF EXISTS our_sequelized_burger_db;
+CREATE DATABASE our_sequelized_burger_db;
+USE our_sequelized_burger_db;
