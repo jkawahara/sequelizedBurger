fedd1cca70dc711b3503d0425edb269aa7ef3a17
Parashara <parashara@gmail.com>
2019-01-04 17:41:49 -0800

    fixed sequelize update

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -30,45 +30,15 @@ router.get("/", function (req, res) {
 });
 // Create operation for post request 
 router.post("/api/burgers", function (req, res) {
-  //   burger.insertOne(["burger_name", "devoured"], [req.body.name, req.body.devoured], function(result) {
-  //     res.json({ id: result.insertId });
-  //   });
-  // });
-  console.log(req.body)
-  // console.log(res)
   db.Burger.create(req.body).then(function () {
-    // console.log(dbBurger)
-    // res.json({ id: "1" });
     res.json({})
   });
 });
 // Update operation for put request
 router.put("/api/burgers/:id", function (req, res) {
-  //   var condition = "id = " + req.params.id;
-  //   burger.updateOne(
-  //     {
-  //       devoured: req.body.devoured
-  //     },
-  //     condition,
-  //     function (result) {
-  //       if (result.changedRows === 0) {
-  //         // Return 404 if no rows changed
-  //         return res.status(404).end();
-  //       }
-  //       res.status(200).end();
-  //     }
-  //   );
-  // });
-  // router.put(‘/book/:bookId’, function (req, res, next) {
-  //   Book.update(
-  //     {title: req.body.title},
-  //     {where: req.params.bookId}
-  //   )
-  console.log(req.params.id)
-  console.log(req.body.devoured)
   db.Burger.update(
     {devoured: req.body.devoured},
-    {where: req.params.id}
+    {where: {id : req.params.id}}
   ).then(function (dbBurger) {
     res.json(dbBurger);
   });
